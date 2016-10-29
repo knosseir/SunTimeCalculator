1d9acfa68eaf3ce55e82d3832cce2713bebfd8f3
Bryson Li <brysonli@gmail.com>
2016-10-29 10:03:52 -0700

    Date.js

---
+++ Date.js
@@ -0,0 +1,10 @@
+// The following code is used to get the current date.  
+// You can use this in other parts of the website!
+
+
+var date = new Date();
+
+// Display the month, day, and year. getMonth() returns a 0-based number.
+var month = dt.getMonth()+1;
+var day = dt.getDate();
+var year = dt.getFullYear();
