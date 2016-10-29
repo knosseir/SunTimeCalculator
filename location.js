0454b23567f8ad95e347d94a514f129f4d42624a
knosseir <knosseir@users.noreply.github.com>
2016-10-29 12:42:13 -0700

    Added some location functionality

--- location.js
+++ location.js
@@ -4,7 +4,21 @@ var startPos;
     startPos = position;
     document.getElementById('startLat').innerHTML = startPos.coords.latitude;
     document.getElementById('startLon').innerHTML = startPos.coords.longitude;
+    var lat = startPos.coords.latitude;
+    var lon = startPos.coords.longitude;
   };
   navigator.geolocation.getCurrentPosition(geoSuccess);
 
 }
+
+function initMap() {
+          var uluru = {lat: -25.363, lng: 131.044};
+          var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 4,
+            center: uluru
+          });
+          var marker = new google.maps.Marker({
+            position: {lat: lat, lon: lon},
+            map: map
+          });
+        }
